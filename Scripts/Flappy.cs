71fb5d3fa117b7d9219c9565469b0d4bdea70c76
Gabriel Rodríguez Hernández <alu0101216829@ull.edu.es>
2023-01-21 12:26:22 +0000

    Update Flappy.cs

--- Scripts/Flappy.cs
+++ Scripts/Flappy.cs
@@ -1,39 +1,44 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-public class Flappy : MonoBehaviour
-{
+[RequireComponent(typeof (Rigidbody2D))]
+public class Flappy : MonoBehaviour {
+    /// <value name="jumpSpeed"> Float which indicates the jump speed </value>
     public float jumpSpeed;
 
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
 
-    private void Start()
-    {
+    /// <summary>
+        /// boot function in which  the rigidbody of our blooper is collected
+    /// </sumary>
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    /// <value name="jumpRequest"> Lets you know if the blooper should jump or not </value>
     public bool jumpRequest;
-    private void Update()
-    {
-        if (rb.velocity.y > 0)
-        {
+
+    /// <summary>
+        /// Update function we rotate the orientation of the blooper when he jumps.
+        /// it depends on the speed.
+    /// </sumary>
+    private void Update() {
+        if (rb.velocity.y > 0) {
             transform.eulerAngles = new Vector3(0, 0, 45);
-        }
-        else if (rb.velocity.y < 0)
-        {
+        } else if (rb.velocity.y < 0) {
             transform.eulerAngles = new Vector3(0, 0, -45);
-        }
-        else
-        {
+        } else {
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
     }
-    private void FixedUpdate()
-    {
-        if (jumpRequest)
-        {
-            jumpRequest = false;
-            rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
-        }
+
+    /// <summary>
+        /// Check if the blooper wants to jump. If a jump is requested, the
+        /// blooper jumps (adding an up force on the rigidbody).
+    /// </sumary>
+    private void FixedUpdate() {
+        if (!jumpRequest) return;
+        jumpRequest = false;
+        rb.velocity = Vector2.zero;
+        rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
     }
 }
