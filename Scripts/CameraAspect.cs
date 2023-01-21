6224940966018ab56bd8f17908107117cae1ae72
Gabriel Rodríguez Hernández <alu0101216829@ull.edu.es>
2023-01-21 12:26:00 +0000

    Update CameraAspect.cs

--- Scripts/CameraAspect.cs
+++ Scripts/CameraAspect.cs
@@ -1,17 +1,17 @@
-﻿using UnityEngine;
-namespace EvolutionaryPerceptron.Util
-{
-    public class CameraAspect : MonoBehaviour
-    {
+using UnityEngine;
+namespace EvolutionaryPerceptron.Util {
+    public class CameraAspect : MonoBehaviour {
+        /// <value> The target camera the ratio between height and width </value>
         public float aspect;
 
-        void Start()
-        {
+        /// <summary>
+            /// Modify the main camera aspect to the specific ratio
+        /// </summary>
+        void Start() {
             var variance = aspect / Camera.main.aspect;
             if (variance < 1.0f)
                 Camera.main.rect = new Rect((1.0f - variance) / 2.0f, 0, variance, 1.0f);
-            else
-            {
+            else {
                 variance = 1.0f / variance;
                 Camera.main.rect = new Rect(0, (1.0f - variance) / 2.0f, 1.0f, variance);
             }
