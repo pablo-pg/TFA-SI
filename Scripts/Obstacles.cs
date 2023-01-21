1f19c2eb495918f4b1cc85d868c8440c5fe0e85c
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2023-01-21 11:38:00 +0000

    doc: obstacles and score system docummented

--- Scripts/Obstacles.cs
+++ Scripts/Obstacles.cs
@@ -1,32 +1,44 @@
 ﻿using UnityEngine;
 
-public class Obstacles : MonoBehaviour
-{
-    public float speed;
-    public Vector2 yRange = new Vector2(5, -1.3f);
-    public float jumpPostion = 11;
+public class Obstacles : MonoBehaviour {
+    [Tooltip("The speed at which the obstacles move")]
+    [SerializeField] private float speed;
+
+    [Tooltip("The x position where the obstacles will be spawned")]
+    [SerializeField] private float xDistance = 15;
+
     public Transform center;
 
-    Vector3 startPoint;
+    /// <value> Representing the possible 'Y' positions. </value>
+    private Vector2 yRange = new Vector2(5, -1.3f);
 
-    private void Start()
-    {
+    /// <value> The initial position. </value>
+    private Vector3 startPoint;
+
+    /// <summary>
+        /// Set the start point of the obstacles.
+    /// </summary>
+    private void Start() {
         startPoint = transform.position;
     }
 
-    private void Update()
-    {
+    /// <summary>
+        /// Move the obstacles to the left and reset their position when they reach the end.
+    /// </summary>
+    private void Update() {
         transform.position += Vector3.left * speed * Time.deltaTime;
 
-        if (transform.position.x < -6)
-        {
-            var pos = transform.position + Vector3.right * jumpPostion;
-            pos.y = Random.Range(yRange.x, yRange.y);
-            transform.position = pos;
-        }
+        if (transform.position.x >= -6) return;
+
+        Vector3 newPosition = transform.position + Vector3.right * xDistance;
+        newPosition.y = Random.Range(yRange.x, yRange.y);
+        transform.position = newPosition;
     }
-    public void ReturnToStart()
-    {
+
+    /// <summary>
+        /// Reset the position of the obstacles.
+    /// </summary>
+    public void ReturnToStart() {
         transform.position = startPoint;
     }
 }
--- Scripts/ScoreSystem.cs
+++ Scripts/ScoreSystem.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using TMPro;
 
-
 class ScoreSystem : MonoBehaviour {
-    public FlappyMendelMachine notifier;
+    [Tooltip("The notifier that will tell us when to reset the score")]
+    [SerializeField] private FlappyMendelMachine notifier;
+
     [Tooltip("The text that displays the score")]
     [SerializeField] private TMP_Text _scoreText;
-    float currentTime  =  0.0f;
+
+    /// <value> How much time has passed since the last pipe. </value>
+    float currentTime = 0.0f;
 
     /// <value> The current score of the gulp. </value>
     private int _score = 0;
